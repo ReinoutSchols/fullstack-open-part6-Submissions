0f0bb1bd4316771b24675cfda7a3b8a5d6fa8c05
Reinout Schols <reinoutschols@hotmail.com>
2024-01-19 12:05:13 +0100

    completed exercise 6.5: anecdotes, step3

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -4,7 +4,7 @@ import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
 const App = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state)
- 
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
 
   const vote = (id) => {
     console.log('vote', id)
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
